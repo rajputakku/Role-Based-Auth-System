76e343319eab161f73e48066fe6419110278b38a
Apple <apple@Apples-MacBook-Air.local>
2020-11-09 08:42:11 +0530

    final changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 global.toastr = require("toastr");
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
